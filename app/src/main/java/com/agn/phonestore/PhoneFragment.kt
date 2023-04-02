0b01d42b714419f0a92ed409f85175e51a93cadc
Argen Bagylov <121685247+AGNARE@users.noreply.github.com>
2023-04-02 13:56:45 +0600

    exemple 2

---
+++ app/src/main/java/com/agn/phonestore/PhoneFragment.kt
@@ -0,0 +1,20 @@
+package com.agn.phonestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PhoneFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_phone, container, false)
+    }
+}
